a9c5d8c53a13c8a1896fb3997b32ada96ecfe8fa
Jerson Suaza <jerson.suaza@gmail.com>
2022-11-05 11:28:02 -0500

    user module

---
+++ controllers/usersCtrl.js
@@ -0,0 +1,26 @@
+const {registerUser, loginUser, infoUser} = require('./../services/usersService');
+
+const signup = async (req, res) => {
+    const user = req.body;
+    const { response, httpStatus } = await registerUser(user);
+    res.status(httpStatus).json(response);
+}
+
+const login = async (req, res) => {
+    const user = req.body; //{ email: "", password: "" }
+    const { response, httpStatus } = await loginUser(user);
+    res.status(httpStatus).json(response);
+}
+
+const info = async (req, res) => {
+    const id = ""; // TODO: pendiente...
+    const { response, httpStatus } = await infoUser(id);
+    res.status(httpStatus).json(response);
+}
+
+module.exports = {
+    login, 
+    signup, 
+    info
+}
+
